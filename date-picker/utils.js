21a6d6727e7008d2010be7bf22ddfadab223a009
vijays-hub <vijayskumar82@gmail.com>
2024-10-06 13:26:06 +0530

    Tweak: Adding a comment to explain checkClassExists function

--- date-picker/utils.js
+++ date-picker/utils.js
@@ -1,5 +1,13 @@
 function checkClassExists(elements, className) {
-  //   Check if classList exists and if it contains the class name being passed.
+  console.log({ elements, className });
+
+  /**
+   * Each element in the elements array is a DOM element. We need to check if
+   * this element has a classList property. If it does, we check if it contains
+   * the className being passed. If it does, we return true. If it doesn't, we
+   * return false.
+   */
+
   return elements.some(
     (element) => element.classList && element.classList.contains(className)
   );
