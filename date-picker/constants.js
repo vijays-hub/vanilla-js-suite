3ed1cfc64f0529be5f9110f95109ec392e49506e
vijays-hub <vijayskumar82@gmail.com>
2024-10-06 15:55:16 +0530

    Tweak: Moving all the selectors to a discrete constants file

---
+++ date-picker/constants.js
@@ -0,0 +1,18 @@
+// All the DOM elements
+const date_picker_wrapper = document.querySelector(".date-picker-wrapper");
+const selected_date_element = document.querySelector(".selected-date");
+const dates_container = document.querySelector(".dates-container");
+const month_element = document.querySelector(".month .month-item");
+const next_month_element = document.querySelector(".month .next-month");
+const prev_month_element = document.querySelector(".month .prev-month");
+const days_container = document.querySelector(".days-container");
+
+export {
+  date_picker_wrapper,
+  selected_date_element,
+  dates_container,
+  month_element,
+  next_month_element,
+  prev_month_element,
+  days_container,
+};
