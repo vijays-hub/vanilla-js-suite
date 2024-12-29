55433e2aeddc922ed72cd28d2ef6ded7fe19fa2a
vijays-hub <vijayskumar82@gmail.com>
2024-12-29 12:08:05 +0530

    JS Suite: Drag and Drop - Adding a provision to drag and drop an image anywhere in the grid!

--- drag-drop-swap-add-delete/script.js
+++ drag-drop-swap-add-delete/script.js
@@ -2,6 +2,61 @@ const gridContainer = document.querySelector(".grid_container");
 
 const GRID_SIZE = 3;
 
+// This will be used to keep track of the dragged item
+let draggedItem = null;
+
+// This will be used to keep track of the parent of the dragged item
+let draggedItemParent = null;
+
+function onFileDragStart(event) {
+  draggedItem = this; // this refers to the image wrapper
+  draggedItemParent = this.parentNode; // this refers to the grid item
+
+  /**
+   * When we start dragging an image, we will set the visibility of the image wrapper
+   * to hidden. This way, user can feel that the image is being dragged from this grid item
+   * to another. Also, let's do that inside a setTimeout function so that the browser can
+   * register the drag event properly. Else, the image will be hidden immediately and you won't
+   * be able to see the image being dragged. Try yourself by uncommenting the line below and
+   * commenting the setTimeout function.
+   */
+  // this.style.visibility = "hidden";
+  setTimeout(() => (this.style.visibility = "hidden"), 0);
+}
+
+function onFileDragEnd(event) {}
+
+function onFileDragOver(event) {
+  event.preventDefault(); // This helps to stop the dragover event from bubbling up to the parent element!
+
+  // The dragover will be called many times, so let's make sure we are adding the class only once.
+  if (!this.classList.contains("hovered")) {
+    this.classList.add("hovered");
+  }
+}
+
+function onFileDragEnter(event) {}
+
+function onFileDragLeave(event) {
+  /**
+   * When the dragged item leaves the grid item, we will remove the hovered class from the grid item.
+   * This is essentially the only purpose of this function.
+   */
+  this.classList.remove("hovered");
+}
+
+function onFileDrop(event) {
+  /**
+   * When a file is dropped on a grid item, we will do a couple of things:
+   * 1. Remove the hovered class from the grid item, so that the user can see that the image is dropped.
+   * 2. Mark the visibility of the dragged item to visible, so that the user can see the image being dropped.
+   * 3. Append the dragged item to the grid item where the file is dropped.
+   */
+  this.classList.remove("hovered");
+  draggedItem.style.visibility = "visible";
+  this.appendChild(draggedItem);
+}
+
 function onDeleteIconClick(imageWrapper, file) {
   /**
    * The idea is to simply remove the image wrapper from the grid item. Additionally,
@@ -56,6 +111,10 @@ function appendImageToGridItem(file, gridItem) {
   if (selectedFile) {
     reader.readAsDataURL(selectedFile);
     imageWrapper.appendChild(image);
+
+    // Add the drag/drop event listeners
+    imageWrapper.addEventListener("dragstart", onFileDragStart);
+    imageWrapper.addEventListener("dragend", onFileDragEnd);
   }
 
   gridItem.appendChild(imageWrapper);
@@ -85,6 +144,17 @@ function generateGridLayout() {
 
     gridItem.appendChild(addIcon);
     gridItem.appendChild(file);
+
+    /**
+     * Each Grid Item should respond to couple of events related to the drag and drop,
+     * like dragover, dragenter, dragleave, and drop. We can make use of these events
+     * for our utility functions like swapping the images, styling a grid item when
+     * an image is dragged over it, etc.
+     */
+    gridItem.addEventListener("dragover", onFileDragOver);
+    gridItem.addEventListener("dragenter", onFileDragEnter);
+    gridItem.addEventListener("dragleave", onFileDragLeave);
+    gridItem.addEventListener("drop", onFileDrop);
 
     gridContainer.appendChild(gridItem);
   }
