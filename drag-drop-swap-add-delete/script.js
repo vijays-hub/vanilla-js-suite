4cabb6dcadaf8dc4566fc04e75e51ce2268909a5
vijays-hub <vijayskumar82@gmail.com>
2024-12-29 11:00:20 +0530

    JS Suite: Drag and Drop - Generating the Grid layout and styling it!

--- drag-drop-swap-add-delete/script.js
+++ drag-drop-swap-add-delete/script.js
@@ -1,1 +1,29 @@
-console.log('Script loaded');
+const gridContainer = document.querySelector(".grid_container");
+
+const GRID_SIZE = 3;
+
+function generateGridLayout() {
+  gridContainer.style.setProperty("--grid-size", GRID_SIZE);
+
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid_item");
+
+    // The add icon to show that the user can add an image here
+    const addIcon = document.createElement("div");
+    addIcon.classList.add("add_icon");
+    addIcon.textContent = "+";
+
+    // The file input element to select an image
+    const file = document.createElement("input");
+    file.type = "file";
+    file.className = "file";
+
+    gridItem.appendChild(addIcon);
+    gridItem.appendChild(file);
+
+    gridContainer.appendChild(gridItem);
+  }
+}
+
+generateGridLayout();
